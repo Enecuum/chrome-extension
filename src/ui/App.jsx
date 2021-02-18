3325502e931083aaf1feabf81e8359b8f6202039
mpakunderscore <mpakunderscore@gmail.com>
2021-02-18 18:16:26 +0300

    Update UI for CSS

--- src/ui/App.jsx
+++
@@ -1,7 +0,0 @@
-import React, {Component, Fragment} from 'react'
-
-// @observer // У Компонета с этим декоратом будет автоматически вызван метод render, если будут изменены observable на которые он ссылается
-export default class App extends Component {
-
-}
-
