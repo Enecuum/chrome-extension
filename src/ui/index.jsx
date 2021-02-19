fb8f7c36bd80f35f9f9cf1d578ad90f3bd3f9fd3
mpakunderscore <mpakunderscore@gmail.com>
2021-02-19 17:26:56 +0300

    UI update

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React from 'react'
 import {render} from 'react-dom'
-import styles from './index.module.css';
+import styles from './index.module.css'
 
 function App() {
-    return (
-        <Login/>
-    )
+    return <Main/>
+}
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLogin: false}
+        this.login = this.login.bind(this)
+        this.logout = this.logout.bind(this)
+    }
+
+    login() {
+        this.setState({isLogin: true});
+    }
+
+
+    logout() {
+        this.setState({isLogin: false});
+    }
+
+    render() {
+        if (this.state.isLogin)
+            return <Account logout={this.logout}/>
+        else
+            return <Login login={this.login}/>
+    }
 }
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.submit = this.submit.bind(this);
-        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this)
+        this.submit = this.submit.bind(this)
+        this.state = {
+            value: '',
+        }
     }
 
     handleChange(e) {
@@ -21,39 +46,54 @@ class Login extends React.Component {
     }
 
     submit() {
-        console.log(this.state.value)
+        this.props.login()
     }
 
     render() {
         return (
             <div>
-                <input
-                    id='markdown-content'
-                    onChange={this.handleChange}
-                    defaultValue={this.state.value}
-                    placeholder={'Private key'}
-                    className={styles.field}
+                <input type="text"
+                       onChange={this.handleChange}
+                       value={this.state.value}
+                       className={styles.field}
+                       placeholder={'Private Key'}
                 />
-                <div onClick={this.submit}
-                     className={[styles.field, styles.button]}>Submit
+
+                <div className={styles.title}>Enecuum Network</div>
+                <div className={styles.text}>Devices connect to the Enecuum blockchain and share untapped data
+                    processing capacity. The more devices connected, the higher the network speed, with uncapped
+                    scalability potential. Connecting millions of distributed devices will create a truly decentralized,
+                    secure and stable system.
                 </div>
-                <div
-                className={styles.red}>
-                    hello world
+
+                <div onClick={this.submit}
+                     className={styles.field + ' ' + styles.button}>Submit
                 </div>
             </div>
         )
     }
 }
 
-function Account() {
-    return (
-        <div>
-            <div>Public key</div>
-            <div>KEY</div>
-            <div>Balance:</div>
-        </div>
-    )
+class Account extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: 'test',
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={styles.field + ' ' + styles.text}>Public key</div>
+                <div className={styles.field + ' ' + styles.text}>KEY</div>
+                <div className={styles.field + ' ' + styles.text}>Balance:</div>
+                <div onClick={this.props.logout}
+                     className={styles.field + ' ' + styles.button}>Logout
+                </div>
+            </div>
+        )
+    }
 }
 
 export async function initApp(background) {
