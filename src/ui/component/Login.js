7e23384dbb763e4403af476f9e8e56ff985a858d
kamighost1 <romires2000@gmail.com>
2021-03-03 00:28:47 +0300

    add gitignore

--- src/ui/component/Login.js
+++ src/ui/component/Login.js
@@ -1,27 +1,40 @@
 import React from "react";
 import styles from "../index.module.css";
+import Network from "./Network"
 
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
+            isNetwork: false,
+            net: 'bit'
         }
         this.handleChange = this.handleChange.bind(this)
         this.submit = this.submit.bind(this)
         this.generate = this.generate.bind(this)
         this.network = this.network.bind(this)
+        this.setNetwork = this.setNetwork.bind(this)
+        this.setNet = this.setNet.bind(this)
     }
 
     handleChange(e) {
-        this.setState({value: e.target.value});
+        this.setState({ value: e.target.value });
+    }
+
+    setNetwork(value) {
+        this.setState({ isNetwork: value })
+    }
+
+    setNet(value) {
+        this.setState({ net: value })
     }
 
     async submit() {
         let publicKey = ENQWeb.Utils.Sign.getPublicKey(this.state.value, true)
         if (publicKey) {
             console.log(publicKey)
-            let user = global.disk.user.addUser(publicKey, this.state.value, 'bit')
+            let user = global.disk.user.addUser(publicKey, this.state.value, this.state.net)
             this.props.login(user)
         }
     }
@@ -31,48 +44,54 @@ export default class Login extends React.Component {
     }
 
     network() {
+        this.setState({ isNetwork: true })
     }
 
     render() {
-        return (
-            <div className={styles.main}>
+        if (this.state.isNetwork) {
+            return <Network setNetwork={this.setNetwork} setNet={this.setNet} net={this.state.net}/>
+        } else {
+            return (
+                <div className={styles.main}>
 
-                {/*<div className={styles.header}>*/}
-                {/*    <div className={styles.title}>Enecuum Network</div>*/}
-                {/*</div>*/}
+                    {/*<div className={styles.header}>*/}
+                    {/*    <div className={styles.title}>Enecuum Network</div>*/}
+                    {/*</div>*/}
 
-                {/*<div></div>*/}
+                    {/*<div></div>*/}
 
-                {/*<div className={styles.title}>Enecuum Network</div>*/}
-                {/*<div className={styles.text}>Devices connect to the Enecuum blockchain and share untapped data*/}
-                {/*    processing capacity. The more devices connected, the higher the network speed, with uncapped*/}
-                {/*    scalability potential. Connecting millions of distributed devices will create a truly decentralized,*/}
-                {/*    secure and stable system.*/}
-                {/*</div>*/}
+                    {/*<div className={styles.title}>Enecuum Network</div>*/}
+                    {/*<div className={styles.text}>Devices connect to the Enecuum blockchain and share untapped data*/}
+                    {/*    processing capacity. The more devices connected, the higher the network speed, with uncapped*/}
+                    {/*    scalability potential. Connecting millions of distributed devices will create a truly decentralized,*/}
+                    {/*    secure and stable system.*/}
+                    {/*</div>*/}
 
-                <div className={styles.form}>
+                    <div className={styles.form}>
 
-                    <input type="text"
-                           onChange={this.handleChange}
-                           value={this.state.value}
-                           className={styles.field}
-                           placeholder={'Private Key'}
-                    />
+                        <input type="text"
+                            onChange={this.handleChange}
+                            value={this.state.value}
+                            className={styles.field}
+                            placeholder={'Private Key'}
+                        />
 
-                    <div onClick={this.submit}
-                         className={styles.field + ' ' + styles.button}>Login
+                        <div onClick={this.submit}
+                            className={styles.field + ' ' + styles.button}>Login
+                        </div>
+
+                        <div onClick={this.generate}
+                            className={styles.field + ' ' + styles.button}>Generate
+                        </div>
+
+                        <div onClick={this.network}
+                            className={styles.field + ' ' + styles.button + ' ' + styles.disabled}>Network: {this.state.net}
+                        </div>
+
                     </div>
-
-                    <div onClick={this.generate}
-                         className={styles.field + ' ' + styles.button}>Generate
-                    </div>
-
-                    <div onClick={this.network}
-                         className={styles.field + ' ' + styles.button}>Network: BIT
-                    </div>
-
                 </div>
-            </div>
-        )
+            )
+        }
+
     }
 }
