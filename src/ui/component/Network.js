a91df1d55a5aa2e3041c9b1460019c17a2c6b38c
kamighost1 <romires2000@gmail.com>
2021-03-03 00:28:47 +0300

    add gitignore

--- src/ui/component/Network.js
+++ src/ui/component/Network.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Transaction} from "./Transaction";
 import styles from "../index.module.css";
 
 export default class Network extends React.Component {
@@ -13,15 +12,20 @@ export default class Network extends React.Component {
             <div className={styles.main}>
 
                 <div className={styles.form}>
+                    
+                    <div className={styles.field + ' ' + styles.balance}>Network: {this.props.net}</div>
 
-                    <div onClick={() => this.selectNetwork('PULSE')} className={styles.field + ' ' + styles.button}>PULSE</div>
+                    <div onClick={() => { this.props.setNet('pulse') }}
+                        className={styles.field + ' ' + styles.button}>PULSE</div>
 
-                    <div onClick={() => this.selectNetwork(true)} className={styles.field + ' ' + styles.button}>F3</div>
+                    <div onClick={() => { this.props.setNet('f3') }}
+                        className={styles.field + ' ' + styles.button}>F3</div>
 
-                    <div onClick={() => this.selectNetwork(true)} className={styles.field + ' ' + styles.button}>BIT</div>
+                    <div onClick={() => { this.props.setNet('bit') }}
+                        className={styles.field + ' ' + styles.button}>BIT</div>
 
-                    <div onClick={() => this.props.setNetwork(false)}
-                         className={styles.field + ' ' + styles.button}>&laquo; Back
+                    <div onClick={() => { this.props.setNetwork(false) }}
+                        className={styles.field + ' ' + styles.button}>&laquo; Back
                     </div>
 
                 </div>
