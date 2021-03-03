e9468b0fcc1b2ea4b50587093c1265c0428acbce
KamiGhost1 <44004821+KamiGhost1@users.noreply.github.com>
2021-03-04 00:01:47 +0300

    add syncTask in Request

--- src/ui/component/Requests.js
+++ src/ui/component/Requests.js
@@ -4,20 +4,49 @@ import styles from "../index.module.css";
 export default class Requests extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            url: '',
+            type: '',
+            taskId: ''
+        }
+        // this.syncRequest = this.syncRequest.bind(this)
         this.allow = this.allow.bind(this)
+        this.disallow = this.disallow.bind(this)
+        console.log(this)
+
+
+        this.syncRequest()
+    }
+
+    syncRequest() {
+        let task = disk.task.loadTask()
+        let ids = Object.keys(task)
+        if (ids.length > 0) {
+            this.state.url = task[ids[0]].cb.url
+            this.state.taskId = task[ids[0]].cb.taskId
+            this.state.type = task[ids[0]].type
+        }
     }
 
     allow() {
+
     }
 
     disallow() {
+
     }
 
     render() {
-
+        // this.syncRequest()
         return (
             <div className={styles.main}>
+
+                <div className={styles.form + ' ' + styles.header}>
+                    <div className={styles.field + ' ' + styles.text}>{this.state.url}</div>
+
+                    <div className={styles.field + ' ' + styles.text}>{this.state.type}</div>
+
+                </div>
 
                 <div className={styles.form}>
 
@@ -27,6 +56,12 @@ export default class Requests extends React.Component {
 
                     <div onClick={this.disallow}
                          className={styles.field + ' ' + styles.button}>Disallow
+                    </div>
+
+                    <div onClick={() => {
+                        this.props.setRequests(false)
+                    }}
+                         className={styles.field + ' ' + styles.button}>Back
                     </div>
 
                 </div>
