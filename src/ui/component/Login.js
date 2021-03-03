fbe67e652590fec8d5de511cbd27535dcfb36187
mpakunderscore <mpakunderscore@gmail.com>
2021-03-03 19:37:25 +0300

    Some empty files

--- src/ui/component/Login.js
+++ src/ui/component/Login.js
@@ -71,6 +71,7 @@ export default class Login extends React.Component {
                     <div className={styles.form}>
 
                         <input type="text"
+                               spellcheck="false"
                                onChange={this.handleChange}
                                value={this.state.value}
                                className={styles.field}
