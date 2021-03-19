ddf7b096b4b6dd46e04d8cfa70d4b3972a2ae99b
mpakunderscore <mpakunderscore@gmail.com>
2021-03-19 17:43:58 +0300

    Design first iteration

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -6,7 +6,14 @@ export default function Header(props) {
     return (
         <div className={styles.header}>
 
-            <img id={'logo'} src='./128.png'/>
+            <img className={styles.logo} src='./128.png'/>
+
+            <div className={styles.welcome1}>Welcome </div>
+            <div className={styles.welcome1}>to Enecuum</div>
+
+            <div className={styles.welcome2}>Connecting you to network and the</div>
+            <div className={styles.welcome2}>Decentralized Web.</div>
+
 
         </div>
     )
