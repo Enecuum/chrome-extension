f7dfa580b18e865055df849a1280324149f90f7f
mpakunderscore <mpakunderscore@gmail.com>
2021-03-22 18:40:43 +0300

    Design for Account component

---
+++ src/ui/elements/Address.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import styles from "../index.module.css";
+
+export default function Address(props) {
+
+    return (
+        <div className={styles.address_row}>
+
+            <div>· Not connected</div>
+
+            <div>
+                <div>Account 1</div>
+                <div>02c3...2473</div>
+            </div>
+
+        </div>
+    )
+}
