1e5f48dec69e64dfee24e3921915780aa9aa32f1
mpakunderscore <mpakunderscore@gmail.com>
2021-03-22 17:16:48 +0300

    Logo update

--- src/ui/elements/Header.js
+++ src/ui/elements/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
     return (
         <div className={styles.header}>
 
-            <img className={styles.logo} src='./128.png'/>
+            <img className={styles.logo_small} src='./images/logo_white.png'/>
 
         </div>
     )
