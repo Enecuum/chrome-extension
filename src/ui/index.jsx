2ddb3c2bb4ff6f60668326aca186f5e9e2210223
mpakunderscore <mpakunderscore@gmail.com>
2021-03-26 16:44:52 +0300

    Refactoring

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -1,51 +1,14 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Login from './components/Login'
-import Account from './components/Account'
-import styles from './css/index.module.css'
+import Main from "./Main";
 
 function App(background) {
     return <Main background={background}/>
 }
 
-class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.background = props.background.background
-        this.state = {
-            user: global.disk.user.loadUser(),
-            isLogin: global.disk.user.loadUser().publicKey,
-            isLocked: disk.lock.checkLock()
-        }
-        this.login = this.login.bind(this)
-        this.logout = this.logout.bind(this)
-
-        // console.log(global.disk.user.loadUser())
-    }
-
-    login(user) {
-        this.setState({isLogin: true, user: user});
-    }
-
-    logout() {
-        global.disk.user.removeUser()
-        disk.lock.removeLock()
-        global.asyncRequest({reject_all: true})
-        this.setState({isLogin: false});
-    }
-
-    render() {
-        if (!this.state.isLogin && !this.state.isLocked)
-            return <Login login={this.login}/>
-        else
-            return <Account logout={this.logout} user={this.state.user} background={this.background}/>
-
-    }
-}
-
 export async function initApp(background) {
     render(
         <App background={background}/>,
-        document.getElementById('app-content')
+        document.getElementById('app')
     );
 }
