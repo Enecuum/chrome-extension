39b76fc1e501207a22e6a8570d8bbade4282964e
mpakunderscore <mpakunderscore@gmail.com>
2021-03-29 21:57:56 +0300

    Scroll

--- src/ui/components/Login.js
+++ src/ui/components/Login.js
@@ -1,17 +1,16 @@
-import React from "react";
-import styles from "../css/index.module.css";
-import Network from "./Network"
-import Header from "../elements/Header";
+import React from 'react'
+import styles from '../css/index.module.css'
+import Network from './Network'
+import Header from '../elements/Header'
 
 let net = localStorage.getItem('net')
-if (!net)
-    net = 'bit'
+if (!net) net = 'bit'
 
 ENQWeb.Net.provider = net
 
 export default class Login extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             privateKey: '',
             isNetwork: false,
@@ -24,37 +23,37 @@ export default class Login extends React.Component {
     }
 
     handleChangePrivateKey(e) {
-        this.setState({privateKey: e.target.value});
+        this.setState({ privateKey: e.target.value })
     }
 
     setNetwork(value) {
-        this.setState({isNetwork: value})
+        this.setState({ isNetwork: value })
     }
 
     async submit() {
-        let publicKey = ENQWeb.Utils.Sign.getPublicKey(this.state.privateKey, true)
+        const publicKey = ENQWeb.Utils.Sign.getPublicKey(this.state.privateKey, true)
         if (publicKey) {
             console.log(publicKey)
-            let user = global.disk.user.addUser(publicKey, this.state.privateKey, ENQWeb.Net.provider)
+            const user = global.disk.user.addUser(publicKey, this.state.privateKey, ENQWeb.Net.provider)
             this.props.login(user)
         }
     }
 
     generate() {
-        let privateKey = ENQWeb.Utils.generateKey.getByNumber(1)[0].prvkey
-        this.setState({privateKey: privateKey})
+        const privateKey = ENQWeb.Utils.generateKey.getByNumber(1)[0].prvkey
+        this.setState({ privateKey: privateKey })
     }
 
     render() {
         if (this.state.isNetwork) {
-            return <Network setNetwork={this.setNetwork}/>
+            return <Network setNetwork={this.setNetwork} />
         }
 
         return (
             <div className={styles.main}>
 
                 <div className={styles.content}>
-                    <img className={styles.login_logo} src='./images/128.png'/>
+                    <img className={styles.login_logo} src="./images/128.png" />
 
                     <div className={styles.welcome1}>Welcome</div>
                     <div className={styles.welcome1}>to Enecuum</div>
@@ -65,30 +64,35 @@ export default class Login extends React.Component {
 
                 <div className={styles.form}>
 
-                    <input type="text"
-                           spellCheck={false}
-                           onChange={this.handleChangePrivateKey}
-                           value={this.state.privateKey}
-                           className={styles.field}
-                           placeholder={'Private Key'}
+                    <input
+                      type="text"
+                      spellCheck={false}
+                      onChange={this.handleChangePrivateKey}
+                      value={this.state.privateKey}
+                      className={styles.field}
+                      placeholder="Private Key"
                     />
 
-                    <div onClick={this.submit}
-                         className={styles.field + ' ' + styles.button}>Login
+                    <div
+                      onClick={this.submit}
+                      className={`${styles.field} ${styles.button}`}
+                    >
+Login
                     </div>
 
-                    <div onClick={this.generate}
-                         className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Generate
+                    <div
+                      onClick={this.generate}
+                      className={`${styles.field} ${styles.button} ${styles.button_blue}`}
+                    >
+Generate
                     </div>
 
-                    {/*<div onClick={() => this.setNetwork(true)}*/}
-                    {/*     className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Network: {ENQWeb.Net.currentProvider.toUpperCase()}*/}
-                    {/*</div>*/}
+                    {/* <div onClick={() => this.setNetwork(true)} */}
+                    {/*     className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Network: {ENQWeb.Net.currentProvider.toUpperCase()} */}
+                    {/* </div> */}
 
                 </div>
             </div>
         )
     }
-
-
 }
