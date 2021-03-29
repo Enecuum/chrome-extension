ee81f3cbc229d8a54f652a0c7639b411e10ebd15
mpakunderscore <mpakunderscore@gmail.com>
2021-03-29 21:24:32 +0300

    Expand and full window

--- src/ui/elements/Menu.js
+++ src/ui/elements/Menu.js
@@ -1,21 +1,23 @@
-import React, {useState} from "react";
-import styles from "../css/elements.module.css";
+import React, { useState } from 'react'
+import styles from '../css/elements.module.css'
 
 export default function Menu(props) {
+    const [amount, setAmount] = useState(0)
 
-    const [amount, setAmount] = useState(0);
-
+    const expand = () => chrome.tabs.create({ url: 'popup.html' })
+    const window = () => chrome.windows.create({ url: 'popup.html', width: 350 })
 
     return (
         <div className={styles.menu}>
 
-            <div className={styles.lock}><img src={'./icons/9.png'}/></div>
+            <div className={styles.lock}><img src="./icons/9.png" /></div>
             <div className={styles.title}>My accounts</div>
             <div>Account 1</div>
             <div>Account 2</div>
 
             <div className={styles.title}>Settings</div>
-            <div>Expand</div>
+            <div onClick={expand}>Expand</div>
+            <div onClick={window}>Window</div>
             <div>Show in blockchain explorer</div>
             <div>Network: PULSE</div>
             <div>Network: BIT</div>
