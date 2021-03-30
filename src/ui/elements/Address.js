7afce293fadbb8fde92747c75b9fdee8b3cf2512
mpakunderscore <mpakunderscore@gmail.com>
2021-03-30 20:45:24 +0300

    Address fix

--- src/ui/elements/Address.js
+++ src/ui/elements/Address.js
@@ -10,7 +10,7 @@ export default function Address(props) {
 
             <div>
                 <div className={elements.account_name}>Account 1</div>
-                <div className={elements.address_string}>02c3...2473</div>
+                <div className={elements.address_string}>{props.publickKey.substring(0, 5) + '...' + props.publickKey.substring(props.publickKey.length - 3, props.publickKey.length - 1)}</div>
             </div>
 
             <div></div>
