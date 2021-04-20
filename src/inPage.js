4858f12eef715c240af659e678994ecd635db852
mpakunderscore <mpakunderscore@gmail.com>
2021-04-20 15:49:23 +0300

    Small fixes

--- src/inPage.js
+++ src/inPage.js
@@ -6,10 +6,8 @@ function checkLib() {
     if (typeof ENQWeb !== "undefined" || typeof ENQweb3lib !== "undefined") {
         console.log('Enecuum lib connected!')
         global.ENQExt = ext_api
-        // let event = new CustomEvent('ENQConnect',{})
-        // document.dispatchEvent(event)
     } else {
-        // console.log('not found ENQ Web lib.')
+        // console.log('Not found Enecuum Web lib.')
     }
 }
 
