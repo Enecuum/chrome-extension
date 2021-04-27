c400982c8d8821ea0f460332c1e9c16c35283dfa
kamighost1 <romires2000@gmail.com>
2021-04-27 21:03:27 +0300

    add parse data field

--- src/ui/components/requests/TransactionRequest.js
+++ src/ui/components/requests/TransactionRequest.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from "../../css/index.module.css";
 import Separator from "../../elements/Separator";
 import elements from "../../css/elements.module.css";
@@ -6,140 +6,155 @@ import elements from "../../css/elements.module.css";
 let fee = 0.1
 const copyText = ('\n\nCopy address to clipboard').toUpperCase()
 
-export default class TransactionRequest extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            url: `${ENQWeb.Net.provider}/#!/tx/` + this.props.txHash,
-            amount: this.props.request.tx.value,
-            data: JSON.stringify(this.props.request.tx.data),
-            from: JSON.stringify(this.props.request.tx.from).replaceAll('"', ''),
-            ticker: this.props.request.tx.tokenHash,
-            to: this.props.request.tx.to,
-            nonce: this.props.request.tx.nonce,
-            activeTab: 0,
-            taskId: this.props.request.cb.taskId,
+
+
+export default function TransactionRequest(props){
+
+    const [activeTab, setActiveTab] = useState(0)
+    const [url, setUrl] = useState(`${ENQWeb.Net.provider}/#!/tx/` + props.txHash)
+    const [data, setData] = useState(props.request.tx.data)
+    const [from, setFrom] = useState(JSON.stringify(props.request.tx.from).replaceAll('"', ''),)
+    const [ticker, setTicker] = useState(props.request.tx.tokenHash)
+    const [to, setTo] = useState(props.request.tx.to)
+    const [amount, setAmount] = useState(props.request.tx.value)
+    const [nonce, setNonce] = useState(props.request.tx.nonce)
+    const [taskId, setTaskId] = useState(props.request.cb.taskId)
+    const [dataText, setDataText] = useState('')
+
+
+    const copyHash = () =>{
+        navigator.clipboard.writeText(props.txHash)
+    }
+
+    const confirm = async ()=>{
+        await global.asyncRequest({allow: true, taskId: taskId})
+        this.props.setTransactionRequest(false)
+    }
+
+    const parseData = ()=>{
+        let dataText;
+        let field = data
+        if(ENQWeb.Utils.ofd.isContract(field)){
+            field = ENQWeb.Utils.ofd.parse(field);
+            if(field.type){
+                dataText='Type: ' + field.type + '\n';
+            }
+            if(field.parameters){
+                for(let key in field.parameters){
+                    // console.log(key, field.parameters[key]);
+                    dataText+=`${key}: ${field.parameters[key]} \n`
+                }
+            }
+        }else{
+            dataText = field;
         }
-
-        console.log(this.props.request)
-        console.log(this.props.taskId)
-
-        this.confirm = this.confirm.bind(this)
-        this.reject = this.reject.bind(this)
+        setDataText(dataText)
     }
 
-    copyHash() {
-        navigator.clipboard.writeText(this.props.txHash)
+    const reject = async ()=>{
+        await global.asyncRequest({disallow: true, taskId: taskId})
+        props.setTransactionRequest(false)
     }
 
-    async confirm() {
-        await global.asyncRequest({allow: true, taskId: this.state.taskId})
-        this.props.setTransactionRequest(false)
-    }
+    useEffect(()=>{
+        parseData()
+    },[])
 
-    async reject() {
-        await global.asyncRequest({disallow: true, taskId: this.state.taskId})
-        this.props.setTransactionRequest(false)
-    }
+    return (
+        <div className={styles.main}>
 
-    render() {
+            <div className={styles.transaction_network}>
+                Network: {ENQWeb.Net.currentProvider.toUpperCase()}
+            </div>
 
-        return (
-            <div className={styles.main}>
+            <div className={styles.data}>
 
-                <div className={styles.transaction_network}>
-                    Network: {ENQWeb.Net.currentProvider.toUpperCase()}
-                </div>
+                <div className={styles.transaction_from_to}>
 
-                <div className={styles.data}>
+                    <div className={styles.transaction_address_copy} onClick={() => {
+                        navigator.clipboard.writeText(from)
+                    }} title={from + copyText}>{shortAddress(from)}</div>
 
-                    <div className={styles.transaction_from_to}>
+                    <div>❯</div>
 
-                        <div className={styles.transaction_address_copy} onClick={() => {
-                            navigator.clipboard.writeText(this.state.from)
-                        }} title={this.state.from + copyText}>{shortAddress(this.state.from)}</div>
-
-                        <div>❯</div>
-
-                        <div className={styles.transaction_address_copy} onClick={() => {
-                            navigator.clipboard.writeText(this.state.to)
-                        }} title={this.state.to + copyText}>{shortAddress(this.state.to)}</div>
-
-                    </div>
-
-                    <div className={styles.transaction_url}>{this.props.request.cb.url}</div>
-
-                    <div className={styles.transaction_type}>TOKEN TRANSFER</div>
-
-                    {/*SWAP TOKEN*/}
-
-                    {/*SWAP EXACT TOKENS FOR*/}
-
-                    {/*<div className={styles.field}>Ticker: {this.state.ticker}</div>*/}
-                    {/*<div className={styles.field}>Nonce: {this.state.nonce}</div>*/}
-                    {/*<div className={styles.field}>Data: {this.state.data}</div>*/}
-                    <div className={styles.transaction_amount}>{(this.state.amount / 1e10) - fee + ' ENQ'}</div>
+                    <div className={styles.transaction_address_copy} onClick={() => {
+                        navigator.clipboard.writeText(to)
+                    }} title={to + copyText}>{shortAddress(to)}</div>
 
                 </div>
 
-                <div className={styles.bottom_tabs + ' ' + styles.data_bottom_tabs}>
-                    <div
-                        onClick={() => this.setState({activeTab: 0})}
-                        className={(this.state.activeTab === 0 ? ` ${styles.bottom_tab_active}` : '')}
-                    >
-                        Details
-                    </div>
-                    <div
-                        onClick={() => this.setState({activeTab: 1})}
-                        className={(this.state.activeTab === 1 ? ` ${styles.bottom_tab_active}` : '')}
-                    >
-                        Data
-                    </div>
-                </div>
+                <div className={styles.transaction_url}>{props.request.cb.url}</div>
 
+                <div className={styles.transaction_type}>TOKEN TRANSFER</div>
+
+                {/*SWAP TOKEN*/}
+
+                {/*SWAP EXACT TOKENS FOR*/}
+
+                {/*<div className={styles.field}>Ticker: {this.state.ticker}</div>*/}
+                {/*<div className={styles.field}>Nonce: {this.state.nonce}</div>*/}
+                {/*<div className={styles.field}>Data: {this.state.data}</div>*/}
+                <div className={styles.transaction_amount}>{(amount / 1e10) - fee + ' ENQ'}</div>
+
+            </div>
+
+            <div className={styles.bottom_tabs + ' ' + styles.data_bottom_tabs}>
                 <div
-                    className={styles.bottom_list + (this.state.activeTab === 0 ? '' : ` ${styles.bottom_list_disabled}`)}>
-
-                    <div className={styles.transaction_data_fee}>
-                        <div>FEE</div>
-                        <div>{fee + ' ENQ'}</div>
-                    </div>
-
-                    <div className={styles.transaction_data_amount}>
-                        <div>TOTAL</div>
-                        <div>{(this.state.amount / 1e10) + ' ENQ'}</div>
-                    </div>
-
+                    onClick={() => setActiveTab(0)}
+                    className={(activeTab === 0 ? ` ${styles.bottom_tab_active}` : '')}
+                >
+                    Details
                 </div>
-
                 <div
-                    className={`${styles.bottom_list} ${this.state.activeTab === 1 ? '' : `${styles.bottom_list_disabled}`}`}>
-
-                    <div
-                        className={styles.transaction_data_data}>{(this.props.request.tx.data ? 'Data' : 'No data')}</div>
-
-                </div>
-
-                <div className={styles.form}>
-
-                    {/*<div onClick={this.allow}*/}
-                    {/*     className={styles.field}>{this.state.website}*/}
-                    {/*</div>*/}
-
-                    <div onClick={this.reject}
-                         className={styles.field + ' ' + styles.button}>Reject
-                    </div>
-
-                    <div onClick={this.confirm}
-                         className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Confirm
-                    </div>
-
-                    <Separator/>
-
+                    onClick={() => setActiveTab(1)}
+                    className={(activeTab === 1 ? ` ${styles.bottom_tab_active}` : '')}
+                >
+                    Data
                 </div>
             </div>
-        )
-    }
+
+            <div
+                className={styles.bottom_list + (activeTab === 0 ? '' : ` ${styles.bottom_list_disabled}`)}>
+
+                <div className={styles.transaction_data_fee}>
+                    <div>FEE</div>
+                    <div>{fee + ' ENQ'}</div>
+                </div>
+
+                <div className={styles.transaction_data_amount}>
+                    <div>TOTAL</div>
+                    <div>{(amount / 1e10) + ' ENQ'}</div>
+                </div>
+
+            </div>
+
+            <div
+                className={`${styles.bottom_list} ${activeTab === 1 ? '' : `${styles.bottom_list_disabled}`}`}>
+
+                <div
+                    className={styles.transaction_data_data}>{(props.request.tx.data ? dataText : 'No data')}</div>
+
+            </div>
+
+            <div className={styles.form}>
+
+                {/*<div onClick={this.allow}*/}
+                {/*     className={styles.field}>{this.state.website}*/}
+                {/*</div>*/}
+
+                <div onClick={reject}
+                     className={styles.field + ' ' + styles.button}>Reject
+                </div>
+
+                <div onClick={confirm}
+                     className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Confirm
+                </div>
+
+                <Separator/>
+
+            </div>
+        </div>
+    )
 }
 
 const shortAddress = (address) => {
