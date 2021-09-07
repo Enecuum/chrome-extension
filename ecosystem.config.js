6f3c40631946628d1c5524a142b30fcf0eb586e9
Pavel <mpakunderscore@gmail.com>
2021-09-07 11:03:06 +0300

    CI update [RN-29]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,16 @@ module.exports = {
             merge_logs: true,
             log_file: './logs/prod.log',
             watch: false
+        },
+        {
+            name: "ci",
+            cwd: "./",
+            script: "ci.js",
+            args: "",
+            exec_mode: "fork",
+            merge_logs: true,
+            log_file: './logs/ci.log',
+            watch: false
         }
     ]
 }
