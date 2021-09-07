5625ed9a1745e1612f9544ae1990d37fad62ee94
Pavel <mpakunderscore@gmail.com>
2021-09-07 12:06:20 +0300

    CI update [RN-29]

--- ci.js
+++ ci.js
@@ -1,17 +1,29 @@
 const http = require('http');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
+const url = require('url');
+
+require('dotenv').config()
 
 http.createServer(function (req, res) {
+
+    const parsedUrl = url.parse(req.url, true)
+    if (parsedUrl.pathname === '/update') {
+        res.writeHead(200, {'Content-type': 'text/plain'})
+        res.write('OK')
+        res.end()
+    }
+
     req.on('data', function (chunk) {
-        let sig = 'sha1=' + crypto.createHmac('sha1', process.env.GITHUB).update(chunk.toString()).digest('hex');
+        let sig = 'sha1=' + crypto.createHmac('sha1', process.env.GITHUB).update(chunk.toString()).digest('hex')
         if (req.headers['x-hub-signature'] == sig) {
-            exec('npm run stop');
-            exec('git pull');
-            exec('npm i --force');
-            exec('npm run make');
-            exec('npm run prod');
+            exec('npm run stop')
+            exec('git fetch --all')
+            exec('git reset --hard origin/master')
+            exec('npm i --force')
+            exec('npm run make')
+            exec('npm run prod')
         }
     });
-    res.end();
-}).listen(process.env.PORT);
+    res.end()
+}).listen(process.env.PORT)
