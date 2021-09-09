d6c6918bd6066cbbd684c15cc1e25faa29eafeca
Pavel <mpakunderscore@gmail.com>
2021-09-09 15:25:47 +0300

    Network done [RN-13]

--- src/ui/components/requests/Network.js
+++ src/ui/components/requests/Network.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from "../../css/index.module.css";
 import Separator from "../../elements/Separator";
-import {regexAddress, shortHash, shortHashLong} from "../../Utils";
+import {regexToken, shortHash} from "../../Utils";
 
 export default function Network(props) {
 
@@ -9,46 +9,87 @@ export default function Network(props) {
     let [host, setHost] = useState('')
     let [hostCorrect, setHostCorrect] = useState(false)
     let [token, setToken] = useState('')
+    let [tokenCorrect, setTokenCorrect] = useState(false)
 
-    let [networks, setNetworks] = useState(Object.entries(ENQWeb.Enq.urls))
+    let [networks, setNetworks] = useState()
+    let [libNetworks, setLibNetworks] = useState([...Object.entries(ENQWeb.Enq.urls)])
+    let [localNetworks, setLocalNetworks] = useState(JSON.parse(localStorage.getItem('networks')) || [])
 
     let checkHost = (host) => {
 
         setHostCorrect(false)
+        setTokenCorrect(false)
 
         let url
         try {
             url = new URL(host);
-            console.log(url)
         } catch (_) {
-            console.log(false)
-            setToken('')
             return false;
         }
 
-        fetch(host + '/api/v1/native_token')
-            .then(response => response.json())
-            .then(data => {
+        try {
+            fetch(host + '/api/v1/native_token')
+                .then(response => response.json())
+                .then(data => {
+                    if (data.hash && data.hash.length > 0) {
+                        setName(data.caption)
+                        setToken(data.hash)
+                        setHostCorrect(true)
+                        setTokenCorrect(true)
+                    }
+                })
+                .catch(e => {
+                    setHostCorrect(false)
+                    fetch(host + '/api/v1/stats')
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.network_hashrate) {
+                                setHostCorrect(true)
 
-                console.log(data)
-
-                if (data.hash && data.hash.length > 0) {
-                    console.log(true)
-                    setName(data.caption)
-                    setToken(data.hash)
-                    setHostCorrect(true)
-                }
-            })
-            .catch(e => {
-                setHostCorrect(false)
-                setToken('')
-            })
+                                if (regexToken.test(token))
+                                    setTokenCorrect(true)
+                            }
+                        })
+                })
+        } catch (e) {
+        }
     }
 
-    let setNet = async (value = this.state.host) => {
+    let addNet = async () => {
 
-        // let value = this.state.host
-        // console.log(value)
+        if (hostCorrect && name.length > 0 && tokenCorrect) {
+        } else {
+            return
+        }
+
+        localNetworks.push({name, host, token})
+        localStorage.setItem('networks', JSON.stringify(localNetworks))
+        setLocalNetworks(localNetworks)
+        renderCards()
+
+        setName('')
+        setHost('')
+        setToken('')
+
+        setHostCorrect(false)
+        setTokenCorrect(false)
+
+        let divCards = document.getElementById('network_cards')
+        divCards.scrollLeft = divCards.scrollWidth - divCards.clientWidth
+    }
+
+    let removeNet = async (name) => {
+
+        let index = localNetworks.findIndex(element => element.name === name)
+        localNetworks.splice(index, 1)
+        setLocalNetworks(localNetworks)
+        localStorage.setItem('networks', JSON.stringify(localNetworks))
+        renderCards()
+    }
+
+    let setNet = async (value) => {
+
+        console.log(value)
 
         localStorage.setItem('net', value)
         ENQWeb.Net.provider = value
@@ -70,41 +111,50 @@ export default function Network(props) {
         })
     }
 
-    let testHost = (host) => {
-        return true
-    }
-
-    let checkURL = () => {
-        // console.log(this.state.url)
-        // ENQWeb.Net.provider = this.state.url
-        // disk.user.setNet(ENQWeb.Net.currentProvider)
-
-        // let connect = false
-        // if (connect) {
-        //     console.log('')
-        // }
-    }
-
-    let getCards = () => {
+    let renderCards = () => {
 
         let cards = []
 
-        for (let i = 0; i < networks.length; i++) {
+        let currentIndex = libNetworks.findIndex(element => element[1] === ENQWeb.Enq.provider)
 
-            let current = ENQWeb.Enq.provider === networks[i][1]
+        libNetworks.unshift(libNetworks.splice(currentIndex, 1)[0])
+
+        for (let i = 0; i < libNetworks.length; i++) {
+
+            let current = ENQWeb.Enq.provider === libNetworks[i][1]
 
             cards.push(
-                <div key={i} className={styles.card + ' ' + (current ? '' : styles.card_select)} onClick={(current ? () => {} : () => setNet(networks[i][1]))}>
-                    <div className={styles.card_title}>{networks[i][1].replace('https://', '').replace('.enecuum.com', '').toUpperCase()}</div>
-                    <div className={styles.card_field}>{networks[i][1]}</div>
-                    <div className={styles.card_field}>{shortHash(ENQWeb.Enq.token[networks[i][1]])}</div>
-                    <div className={styles.card_field_select}>{current ? 'CURRENT' : 'SELECT'}</div>
+                <div key={i + 'lib'} className={styles.card + ' ' + (current ? '' : styles.card_select)}>
+                    <div
+                        className={styles.card_title}>{libNetworks[i][1].replace('https://', '').replace('.enecuum.com', '').toUpperCase()}</div>
+                    <div className={styles.card_field}>{libNetworks[i][1]}</div>
+                    <div className={styles.card_field}>{shortHash(ENQWeb.Enq.token[libNetworks[i][1]])}</div>
+                    <div className={styles.card_field_select} onClick={(current ? () => {} : () => setNet(libNetworks[i][1]))}>{current ? 'CURRENT' : 'SELECT'}</div>
                 </div>
             )
         }
 
-        return cards
+        for (let i = 0; i < localNetworks.length; i++) {
+
+            let current = ENQWeb.Enq.provider === localNetworks[i][1]
+
+            cards.push(
+                <div key={i + 'local'} className={styles.card + ' ' + (current ? '' : styles.card_select)}>
+                    <div className={styles.card_title}>{localNetworks[i].name}</div>
+                    <div className={styles.card_field}>{localNetworks[i].host}</div>
+                    <div className={styles.card_field}>{shortHash(localNetworks[i].token)}</div>
+                    <div className={styles.card_field_select} onClick={(current ? () => {} : () => setNet(localNetworks[i].host))}>{current ? 'CURRENT' : 'SELECT'}</div>
+                    <div className={styles.card_field_delete} onClick={() => removeNet(localNetworks[i].name)}>&#x2715;</div>
+                </div>
+            )
+        }
+
+        setNetworks(cards)
     }
+
+    useEffect(() => {
+        renderCards()
+    }, [])
 
     return (
         <div className={styles.main}>
@@ -122,8 +172,8 @@ export default function Network(props) {
                 <input type="text"
                        spellCheck={false}
                        onChange={async (e) => {
-                           setHost(e.target.value)
-                           await checkHost(e.target.value)
+                           setHost(e.target.value.replace(/\/$/, ""))
+                           await checkHost(e.target.value.replace(/\/$/, ""))
                        }}
                        value={host}
                        className={styles.field + ' ' + (hostCorrect ? styles.field_correct : '')}
@@ -132,28 +182,33 @@ export default function Network(props) {
 
                 <input type="text"
                        spellCheck={false}
-                       onChange={(e) => setToken((e.target.value))}
+                       onChange={(e) => {
+                           setToken((e.target.value))
+                           setTokenCorrect(regexToken.test(e.target.value))
+                       }}
                        value={token}
-                       disabled={hostCorrect}
-                       className={styles.field + ' ' + (hostCorrect ? styles.field_correct : '')}
+                    // disabled={tokenCorrect}
+                       className={styles.field + ' ' + (tokenCorrect ? styles.field_correct : '')}
                        placeholder={'Token, something like 00000...'}
                 />
 
             </div>
 
             <div className={styles.cards_container}>
-                <div className={styles.cards}>
-                    {getCards()}
+                <div className={styles.cards} id={'network_cards'}>
+                    {networks}
                 </div>
             </div>
 
             <div className={styles.form}>
 
-                <div onClick={setNet}
-                     className={styles.field + ' ' + styles.button + ' ' + (hostCorrect ? styles.button_blue : '')}>Set
+                <div onClick={addNet}
+                     className={styles.field + ' ' + styles.button + ' ' + ((hostCorrect && name.length > 0 && tokenCorrect) ? styles.button_blue : '')}>Add
                 </div>
 
-                <div onClick={() => {props.setNetwork(false)}}
+                <div onClick={() => {
+                    props.setNetwork(false)
+                }}
                      className={styles.field + ' ' + styles.button + ' ' + styles.button_blue}>Back
                 </div>
 
