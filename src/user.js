f05df2719728e01aff0073f12b9ea349901263b8
mpakunderscore <mpakunderscore@gmail.com>
2021-11-11 19:47:56 +0300

    User model [RN-62]

--- src/user.js
+++ src/user.js
@@ -1,16 +1,31 @@
 let account = {
+
     net: '',
     token: '',
 
+    // Old type, may be null
     privateKey: '',
     publicKey: '',
 
     mainPrivateKey: '',
     mainPublicKey: '',
+    accountIndex: 0,
 
+    // Always encrypted
     seed: '',
     seedAccountsArray: [0],
+    // Do we have to remember selected private keys from seed?
 
+    // Ledger ID
     ledger: '',
     ledgerAccountsArray: [],
+}
+
+let getSeedAccounts = (password) => {
+
+}
+
+let changeAccount = (index) => {
+    account.mainPrivateKey = [index]
+    account.accountIndex = index
 }
