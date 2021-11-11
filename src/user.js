3ad189577511cecd142f097043e1c5c65ad34f39
mpakunderscore <mpakunderscore@gmail.com>
2021-11-11 19:27:12 +0300

    User model [RN-62]

---
+++ src/user.js
@@ -0,0 +1,16 @@
+let account = {
+    net: '',
+    token: '',
+
+    privateKey: '',
+    publicKey: '',
+
+    mainPrivateKey: '',
+    mainPublicKey: '',
+
+    seed: '',
+    seedAccountsArray: [0],
+
+    ledger: '',
+    ledgerAccountsArray: [],
+}
