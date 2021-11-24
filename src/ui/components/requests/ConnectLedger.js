945ca2c16824fdd8c96abc8daa566c8ebd0c52c0
mpakunderscore <mpakunderscore@gmail.com>
2021-11-24 19:59:44 -0400

    Ledger update

--- src/ui/components/requests/ConnectLedger.js
+++ src/ui/components/requests/ConnectLedger.js
@@ -4,15 +4,8 @@ import Separator from '../../elements/Separator'
 import {shortHashLong} from '../../Utils'
 
 import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
-import Eth from "@ledgerhq/hw-app-eth";
 import {signHash, getVersion, getPublicKey} from '../../../utils/ledgerShell'
 import TransportWebHID from '@ledgerhq/hw-transport-webhid';
-
-// TransportWebUSB.isSupported().then((result) => {
-//     console.log('WebUSB Supported: ' + result)
-// })
-
-// global.WebUSB = TransportWebUSB
 
 export default function ConnectLedger(props) {
 
@@ -27,6 +20,7 @@ export default function ConnectLedger(props) {
     const [eth5Address, setEth5Address] = useState('')
 
     const closeModalWindow = () => {
+
         let params = getUrlVars()
         if (params.type === 'connectLedger') {
             window.close()
@@ -56,12 +50,50 @@ export default function ConnectLedger(props) {
 
     const connectLedger = async () => {
 
-        const transport = await TransportWebHID.create()
-        let ver = await getVersion(transport)
-        let publicKey = await getPublicKey(1, transport)
-        let hash = 'message'
-        let sign = await signHash(hash, 1, transport)
-        console.log({ver, publicKey: publicKey, sign: sign.substr(0, 140)})
+        console.log('connectLedger')
+
+        // const transport = await TransportWebHID.create()
+
+        // let version = await getVersion(transport)
+
+        // TODO We will give to choose one of keys in selector later, now only 1 key
+        // let publicKey = await getPublicKey(1, transport)
+
+        // TODO This is our old user model, but for now it's ok to use it
+        let data = {
+            // publicKey: publicKey,
+            // TODO this is fake publick key because i still not install app in my ledger
+            publicKey: '02c3143abeb50e4153da372868490277c14b2877f05b477e4671722152b0112473',
+            privateKey: true,
+            net: ENQWeb.Net.provider,
+            token: ENQWeb.Enq.ticker,
+            ledger: true,
+        }
+
+        // TODO :( I want some rest and money
+        // I want some water and honey
+        // You want to write some code?
+        // Will i be at better spot
+
+        global.disk.promise.sendPromise({
+            account: true,
+            set: true,
+            data: data
+        }).then(r => {
+            props.login(data)
+            window.close()
+        })
+
+        // let hash = 'message'
+        // let sign = await signHash(hash, 1, transport)
+        //
+        // console.log(
+        //     {version, publicKey: publicKey, sign: sign.substr(0, 140)}
+        // )
+
+
+
+
 
         // let transport = await props.getLedgerTransport()
         //
@@ -102,9 +134,8 @@ export default function ConnectLedger(props) {
 
 
     useEffect(() => {
-        checkDevice()
-        connectLedger().then(r => {
-        })
+        // checkDevice()
+        // connectLedger().then(r => {})
     }, [])
 
     //
