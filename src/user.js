30dedb1374de2450e82d2c24b8d6ce528134f2bf
mpakunderscore <mpakunderscore@gmail.com>
2021-12-09 19:16:13 -0500

    User model update [RN-62]

--- src/user.js
+++ src/user.js
@@ -1,3 +1,5 @@
+import indexDB from "./indexDB";
+
 let account = {
 
     net: '',
@@ -5,8 +7,8 @@ let account = {
 
     privateKeys: [], // keys
 
-    mainPrivateKey: '',
-    mainPublicKey: '',
+    privateKey: '',
+    publicKey: '',
     type: 0, // 0 - private, 1 - seed, 2 - ledger
     accountIndex: 0,
 
@@ -21,12 +23,41 @@ let account = {
 }
 
 let getSeedAccounts = (password) => {
-
+    return account.seedAccountsArray;
 }
 
 let changeAccount = (type, index) => {
-    let array = []
-    account.mainPrivateKey = array[index]
+    let array;
+    if (type === 0)
+        array = account.privateKeys[index]
+    if (type === 1)
+        array = account.seedAccountsArray[index]
+    if (type === 2) {
+        //TODO set public key
+        // array = account.ledgerAccountsArray[index]
+    }
+    account.mainPrivateKey = array[index];
     account.accountIndex = index
     account.type = type
 }
+
+let addAccountOldFormat = (data)=>{
+    indexDB.get('user').then(account=>{
+        account.mainPublicKey = data.publicKey
+        account.mainPrivateKey = data.privateKey
+        account.token = data.token
+        account.net = data.net
+        account.type = 0
+        account.privateKeys.push(data.privateKey)
+        indexDB.set('user', account).then()
+    })
+}
+let updateAccount = (data)=>{
+    indexDB.get('user').then(account=>{
+        account.net = data.net
+        account.token = data.token
+        indexDB.set('user', account).then()
+    })
+}
+
+export {account, changeAccount, getSeedAccounts, addAccountOldFormat, updateAccount}
