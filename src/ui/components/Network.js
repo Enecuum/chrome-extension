a3309cd6ce79726fd7c68f024a5f2a960c82bf61
kamighost1 <romires2000@gmail.com>
2022-02-01 14:50:04 +0300

    fix: fix death screen

--- src/ui/components/Network.js
+++ src/ui/components/Network.js
@@ -156,7 +156,7 @@ export default function Network(props) {
                 })
             })
 
-        cacheTokens().then(() => {
+        await cacheTokens().then(() => {
             location.reload(false)
         })
     }
