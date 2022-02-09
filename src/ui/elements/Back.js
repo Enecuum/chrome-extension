2bfccbb86d232f1030704d7400a83b86d1dd25e4
mpakunderscore <mpakunderscore@gmail.com>
2022-02-09 19:30:57 +0300

    Fix web url view from /index.html to /

--- src/ui/elements/Back.js
+++ src/ui/elements/Back.js
@@ -1,12 +1,16 @@
 import styles from "../css/index.module.css";
 import elements from "../css/elements.module.css";
 import React, {useState} from 'react';
+import {versions} from "../../utils/names";
+
+// We need normal client routing
+let isWeb = chrome.runtime.getManifest().version.includes(versions.WEB)
 
 export default function Back(props) {
 
     return (
         <div className={styles.field} onClick={() => {
-            window.history.pushState({}, document.title, '/index.html')
+            window.history.pushState({}, document.title,  isWeb ? '' : '/index.html')
             props.setFalse()
         }}>
             ❮ Back
